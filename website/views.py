20e1267eed45b0fe9a3fc5df5b55ccc187346527
alirezarazavi3661 <alirezarazavi3661@gmail.com>
2023-07-04 01:53:15 +0330

    create html template
    i created html templates

--- website/views.py
+++ website/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Home_page(request):
-    return HttpResponse('<h1> Home page </h1> ')
+    return render(request,'website/home.html')
 def About_page(request):
-    return HttpResponse('<h1> About page </h1> ')
+    return render(request,'website/about.html')
 def Contact_page(request):
-    return HttpResponse('<h1> Contact page </h1> ')
+    return render(request,'website/contact.html')
